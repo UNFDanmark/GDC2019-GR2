11f487a2f702551a3b07561a7c2a846fa172eadd
Prog <teknisk@game.unf.dk>
2019-07-29 00:25:58 +0200

    Lavede kommeter og begyndte på stjernehimlen

--- GDC2019/Assets/Scripts/Meteor.cs
+++ GDC2019/Assets/Scripts/Meteor.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class Meteor : MonoBehaviour
 {
+    public Vector3 direction= new Vector3();
     // Start is called before the first frame update
     void Start()
     {
-        
+        //direction.z = -Random.Range(2f, 3f);
     }
 
     // Update is called once per frame
     void Update()
     {
+        GetComponent<Rigidbody>().velocity = direction;
         
     }
 }
---
+++ GDC2019/Assets/astroidsspawnerscript.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class astroidsspawnerscript : MonoBehaviour
+{
+    public float time = 4;
+    public float timeleft;
+    public GameObject commet;
+    public float minforward;
+    public float maxforward;
+    public float minsideways;
+    public float maxsideways;
+     
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeleft = time;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeleft = timeleft - Time.deltaTime;
+        if (timeleft <= 0)
+        {
+            timeleft = time;
+            print("spawn commet");
+            int selector = Random.Range(0, 4);
+            if (selector == 0)
+            {
+                float xposition = Random.Range(-40, 40);
+                GameObject Createdcommet = Instantiate(commet, new Vector3(xposition, 0, 90), Quaternion.identity);
+                Createdcommet.GetComponent<Meteor>().direction.z = -Random.Range(minforward, maxforward);
+                Createdcommet.GetComponent<Meteor>().direction.x = Random.Range(minsideways, maxsideways);
+
+            }
+            else if (selector == 1)
+            {
+
+                float xposition = Random.Range(-40, 40);
+                GameObject Createdcommet = Instantiate(commet, new Vector3(xposition, 0, -90), Quaternion.identity);
+                Createdcommet.GetComponent<Meteor>().direction.z = Random.Range(minforward, maxforward);
+                Createdcommet.GetComponent<Meteor>().direction.x = Random.Range(minsideways, maxsideways);
+            }
+            else if (selector == 2 )
+            {
+
+                float zposition = Random.Range(-70,70);
+                GameObject Createdcommet = Instantiate(commet, new Vector3(70,0,zposition), Quaternion.identity);
+                Createdcommet.GetComponent<Meteor>().direction.x = -Random.Range(minforward, maxforward);
+                Createdcommet.GetComponent<Meteor>().direction.z = Random.Range(minsideways, maxsideways);
+            
+        }
+            else if (selector == 3)
+            {
+                float zposition = Random.Range(-70, 70);
+                GameObject Createdcommet = Instantiate(commet, new Vector3(-70, 0, zposition), Quaternion.identity);
+                Createdcommet.GetComponent<Meteor>().direction.x = +Random.Range(minforward, maxforward);
+                Createdcommet.GetComponent<Meteor>().direction.z = Random.Range(minsideways, maxsideways);
+            }
+
+
+        }
+    }
+}
